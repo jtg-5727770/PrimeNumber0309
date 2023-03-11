0d8508cfa43ff0c1551c4a5268fa795dc1d9ce2b
5727770@stu.kmu.ac.kr <5727770@stu.kmu.ac.kr>
2023-03-11 17:33:33 +0900

    소수롸 소수의 개수 추가

--- PrimeNumber0309/main.c
+++ PrimeNumber0309/main.c
@@ -3,14 +3,20 @@
 int main(void) {
 	int i, j;
 	int sum = 0;
+	int num = 0;
+
 	for (i = 2; i <= 100; i++) {
 		for (j = 2; j < i; j++) {
 			if (i % j == 0)
 				break;
 		}
-		if (i == j)
+		if (j == i) {
 			sum += i;
+			num++;
+			printf("%d ", i);
+		}
 	}
+	printf("\n0부터 100까지중 소수의 갯수 :%d\n", num);
 	printf("0부터 100까지중 소수의 합 :%d\n", sum);
 	system("pause");
 	return 0;
